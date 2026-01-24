8a43a3c0d8621a72064b79b08902759f1bc2f411
Enes <purentap@gmail.com>
2026-01-24 18:59:50 +0300

    pi0fast bash script and train bug fix

---
+++ pi0fast_libero.sh
@@ -0,0 +1,20 @@
+MODEL="lstm_img"
+LAMBDA_REGS="1e-3,1e-2,1e-1" #
+SEEDS="0,1,2"
+EPOCHS="200"
+BATCH_SIZE="64"
+LEARNING_RATES="1e-5,1e-4,3e-4,1e-3" 
+DATASET="pi0fast_libero"
+
+
+python train.py -m \
+    model="${MODEL}" \
+    dataset="${DATASET}" \
+    training.use_scheduler=false \
+    training.learning_rate=${LEARNING_RATES} \
+    training.batch_size="${BATCH_SIZE}" \
+    training.lambda_reg="${LAMBDA_REGS}" \
+    wandb.project="${MODEL}_${DATASET}_batch_${BATCH_SIZE}_epochs_${EPOCHS}" \
+    wandb.enabled=true \
+    seed=${SEEDS} \
+    training.n_epochs=${EPOCHS}
