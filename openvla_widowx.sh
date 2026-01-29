61b8e4b6c1563cc4dea01088399a9643b2798905
rafi-abdurrahman <purentap@gmail.com>
2026-01-29 20:08:37 +0300

    openvla widowx bash script

---
+++ openvla_widowx.sh
@@ -0,0 +1,19 @@
+MODEL="fusion_lstmv2"
+LAMBDA_REGS="1e-4,3e-4,1e-3" #
+SEEDS="0,1,2"
+EPOCHS="500"
+BATCH_SIZE="64"
+LEARNING_RATES="1e-3,1e-2,1e-1,1" #1e-5, 1e-4,3e-4,1e-3,1e-5
+DATASET="openvla_widowx"
+
+python train.py -m \
+    model="${MODEL}" \
+    dataset="${DATASET}" \
+    training.use_scheduler=false \
+    training.learning_rate=${LEARNING_RATES} \
+    training.batch_size="${BATCH_SIZE}" \
+    training.lambda_reg="${LAMBDA_REGS}" \
+    wandb.project="${MODEL}_${DATASET}_batch_${BATCH_SIZE}_epochs_${EPOCHS}" \
+    wandb.enabled=true \
+    seed=${SEEDS} \
+    training.n_epochs=${EPOCHS}
