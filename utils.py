f56038233293e27880b20a90182c5da4c239e7d3
rafi-abdurrahman <purentap@gmail.com>
2026-09-16 12:08:32 +0300

    fix

--- utils.py
+++ utils.py
@@ -9,7 +9,6 @@ import natsort
 
 from data import RolloutData
 from model import MLPModel, LSTMModel, FusionLSTMModel, FusionLSTMModel_v2, DoubleLSTMModel
-data_path = "/mnt/mahzen/puren/open_vla_data_all/rollouts/single-foward/libero_10"
 
 model_classes = {
     "mlp": MLPModel,
